18e6693f9ddbd8d615d2106172b4098b63f275f4
Vitor Frango <51138494+VitorFrango@users.noreply.github.com>
2022-12-31 17:16:14 +0000

    Add files via upload

--- S7/S7Exer9.c
+++ S7/S7Exer9.c
@@ -9,24 +9,23 @@ int main (){
     int numero[6];
     int i;
 
-
     printf("Escreva 6 numeros inteiros pares: \n");
-
         for (int i = 0; i < 6; ++i) {
-            printf("A posição [%d] é ", i);
+            //printf("A posição [%d] é ", i);
+
             scanf("%d", &numero[i]);
 
-            if (numero[i] % 2 == 0) {
 
+            if ( numero[i] % 2 == 0) {
                 printf("O numeros itroduzidos foram, %d \n", numero[i]);
 
             } else {
                 printf("O numero introduzido não é par. \n");
             }
         }
-    for (int i = 6; i >= 0 ; --i) {
+    for (int i = 5; i >= 0 ; --i) {
 
-        printf("O numeros itroduzidos foram, %d \n", numero[i]);
+        printf("O numeros itroduzidos por ordem inversa foram, %d \n", numero[i]);
 
     }
 

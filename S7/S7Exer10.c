040a4661a69d6543b2742f9b99609bd72367fef9
Vitor Frango <51138494+VitorFrango@users.noreply.github.com>
2023-01-01 18:59:49 +0000

    Add files via upload

--- S7/S7Exer10.c
+++ S7/S7Exer10.c
@@ -1,3 +1,38 @@
 //
 // Created by Vitor Frango on 31/12/2022.
 //
+
+
+#include <stdio.h>
+
+int main(){
+
+    int notas[15];
+    int media, soma;
+
+    printf("Qunatas notas quer introduzir? \n");
+    scanf("%d", &notas);
+    printf("Escreva as notas dos alunos: \n");
+
+    for (int i = 0; i < 15; ++i) {
+        scanf("%d", &notas[i]);
+
+
+    }
+
+    for (int i = 0; i < 15; ++i) {
+        printf("A notas foram [%d] \n", notas[i]);
+
+    }
+
+    for (int i = 0; i < 15; ++i) {
+        soma += notas[i];
+
+
+    }
+
+    media = soma / 5;
+    printf("A media das notas foi: %d \n", media);
+
+    return 0 ;
+}
---
+++ S7/S7Exer11.c
@@ -0,0 +1,12 @@
+//
+// Created by Vitor Frango on 01/01/2023.
+//
+
+#include <stdio.h>
+
+int main(){
+
+
+    
+    return 0;
+}

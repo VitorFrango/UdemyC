f7464c268e97078b18bb7386221270e83b8df8e2
Vitor Frango <51138494+VitorFrango@users.noreply.github.com>
2023-01-02 00:07:49 +0000

    Add files via upload

--- S7/S7Exer11.c
+++ S7/S7Exer11.c
@@ -6,7 +6,34 @@
 
 int main(){
 
+    float numero[10];
+    int i, soma;
+    int cont = 3;
+    int qts = 0;
 
-    
+    printf("Escreva 10 numneros reais: \n");
+    for (int i = 0; i < cont; ++i) {
+        scanf("%f", &numero[i]);
+
+
+    }
+
+    for (int i = 0; i < cont; ++i) {
+        printf("Os numeros introduzidos foram: [%0.f] \n", numero[i]);
+
+        if (numero[i] < 0) {
+            qts++;
+
+        }
+
+        else if (numero[i] > 0){
+            soma += numero[i];
+
+        }
+    }
+    printf("Escreveu %d numeros negativos \n", qts);
+    printf("A soma dos nunmeros pares é %0.f", soma);
+
+
     return 0;
 }

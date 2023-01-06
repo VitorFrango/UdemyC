fe93cb2a6d776add4ebee22c15da6f78bf0cbd13
Vitor Frango <51138494+VitorFrango@users.noreply.github.com>
2023-01-06 16:42:07 +0000

    Add files via upload

---
+++ S7/S7Exer15.c
@@ -0,0 +1,25 @@
+//
+// Created by Vitor Frango on 05/01/2023.
+//
+#include <stdio.h>
+
+
+int vector[10];
+int igual, compara;
+
+
+int main (){
+
+    printf("Escreva 10 numeros: \n ");
+
+    for (int i = 0; i < 10; ++i) {
+        scanf("%d", &vector[i]);
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        
+
+    }
+
+    return 0;
+}

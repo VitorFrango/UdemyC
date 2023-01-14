f41f15ea77c0882bd1e3d9c178fec47c2ed26867
Vitor Frango <51138494+VitorFrango@users.noreply.github.com>
2023-01-14 17:09:47 +0000

    Add files via upload

--- S7/S7Exer16.c
+++ S7/S7Exer16.c
@@ -4,28 +4,38 @@
 #include <stdio.h>
 
 float vetor[5];
-float tam = sizeof(vetor) / sizeof(int);
+int tam = sizeof(vetor) / sizeof(int);
 int codigo, i ;
+char opcao;
+
 
 int main(){
 
-    printf("Escreva 5 numeros reais: \n");
-    for ( i = 0; i < tam; ++i) {
-        scanf("%.1f", &vetor[i]);
+    do {
+        printf("Escreva 5 numeros reais: \n");
+        for (i = 0; i < tam; ++i) {
+            scanf("%f", &vetor[i]);
 
+        }
+
+        printf("Escreva um vqlor entre 0 e 2: \n");
+        scanf("%d", &codigo);
+
+        if (codigo == 0) {
+            return 0;
+        } else if (codigo == 1) {
+            for (i = 0; i < tam; ++i) {
+                printf("%.1f \n", vetor[i]);
+            }
+        } else if (codigo == 2) {
+            for (i = tam - 1; i >= 0; --i) {
+                printf("%.1f \n", vetor[i]);
+            }
+        }
+
+
+        printf("Deseja continuar? (s/n) : \n");
+        scanf(" %c", &opcao);
     }
-
-    printf("Escreva agora um codigo inteiro: \n");
-    scanf("%d", &codigo);
-
-    for (int i = 0; i < tam; ++i) {
-        printf("%f", vetor[i]);
-
-    }
-    printf("Os valores introduzidos são: \n" );
-    printf("O codigo introduzido é: %d \n", codigo);
-
-
-
-    return 0;
+    while (opcao == 's' || opcao == 'S');
 }

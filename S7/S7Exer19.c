6eef4a68859424de5b27dac3380b089bb01a0327
Vitor Frango <51138494+VitorFrango@users.noreply.github.com>
2023-01-17 22:41:10 +0000

    Add files via upload

--- S7/S7Exer19.c
+++ S7/S7Exer19.c
@@ -3,12 +3,25 @@
 //
 
 #include <stdio.h>
-#define Max  50
+#define Max  5
 
 int vetor[Max];
+int tam = Max;
+int total, parc1, parc2 = 0 ;
 
 
 int main (){
+
+    for (int i = 0; i < tam; ++i) {
+
+             total = vetor[i] + ( 5 * vetor[i]) % (vetor[i] +1);
+
+    }
+    for (int i = 0; i < tam; ++i) {
+        printf("%d %d \n", total, vetor[i]);
+    }
+
+
 
 
 

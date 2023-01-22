a6a3d7c86008a5cadd8c45e7379ec83b420a1715
Vitor Frango <51138494+VitorFrango@users.noreply.github.com>
2023-01-22 12:56:23 +0000

    Add files via upload

---
+++ S7/S7Exer20.c
@@ -0,0 +1,22 @@
+//
+// Created by Vitor Frango on 22/01/2023.
+//
+#include <stdio.h>
+
+
+int vetor[10];
+
+int main(){
+
+    printf("Escreva 10 interios entre 0 e 50");
+    scanf("%d", &vetor[]);
+
+    for (int i = 0; i < 10; ++i) {
+        if (vetor{} >= 0) && (vetor <= 50 ){
+
+        }
+
+    }
+
+    return 0;
+}
